e88d003fdf96d4c05d4754f482e00c7bd4150dc0
Marc Turner <marc.t1967@gmail.com>
2019-11-20 21:57:22 -0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,60 @@
+$(function() {
+    // jQuery goes here...
+    // Uncomment this line to fade out the red box on page load
+    // $(".red-box").fadeOut(2000);
+    // $(".green-box").fadeOut(2000);
+    // $(".red-box").fadeIn(1000);
+    // $(".red-box").fadeTo(1000, 0.5);
+    // $(".red-box").fadeTo(1000, 0.2);
+    // $(".green-box").fadeTo(2000, 0.5);
+    // $(".blue-box").fadeTo(500, 0.8);
+    // $(".blue-box").fadeToggle();
+    // $(".blue-box").fadeToggle();
+    // $(".blue-box").toggle();
+    // $(".blue-box").hide(1000);
+    // $(".blue-box").show(1000);
+    // $(".blue-box").slideUp(2000);
+    // $(".blue-box").slideDown(2000);
+    // $(".blue-box").slideToggle(2000);
+    // $("p").hide();
+    // $("p").slideDown(1000);
+    // $(".blue-box").animate(
+    //     {
+    //         "margin-left": "+=200px"
+    //     },
+    //     1000,
+    //     "linear"
+    // );
+    // $(".blue-box").animate(
+    //     {
+    //         "margin-left": "-=200px"
+    //     },
+    //     1000,
+    //     "linear"
+    // );
+    // $(".red-box").fadeTo(1000, 0.2);
+    // $(".green-box")
+    //     .delay(1000)
+    //     .fadeTo(1000, 0.5);
+    // $(".blue-box")
+    //     .delay(2000)
+    //     .fadeTo(1000, 0.8)
+    //     .fadeOut()
+    //     .delay(500)
+    //     .fadeIn();
+    // $(".red-box").fadeTo(2000, 0.3, function() {
+    //     $(".green-box").slideToggle(1000, function() {
+    //         $(".blue-box").animate(
+    //             {
+    //                 "margin-left": "+=200px"
+    //             },
+    //             2000,
+    //             "linear"
+    //         );
+    //     });
+    // });
+
+    $(".lightbox")
+        .delay(500)
+        .fadeIn(1000);
+});
