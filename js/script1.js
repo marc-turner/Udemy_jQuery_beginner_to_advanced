97d7865004080eb538d8e120cac296522701d68e
Marc Turner <marc.t1967@gmail.com>
2019-11-21 22:24:52 -0500

    jQuery element selectors

--- js/script1.js
+++ js/script1.js
@@ -23,13 +23,49 @@ $(function() {
     // $("#list")
     //     .next()
     //     .css("background-color", "rgba(180, 180, 30, 0.8)");
-
     //select all the direct next elements that come after a header element
-    $("h2")
-        .next()
-        .css("background-color", "rgba(180, 180, 30, 0.8)");
+    // $("h2")
+    //     .next()
+    //     .css("background-color", "rgba(180, 180, 30, 0.8)");
     // select all the input child elements of the form element which are a password
-    $("form")
-        .children("input[type = 'password']")
+    // $("form")
+    //     .children("input[type = 'password']")
+    //     .css("background-color", "rgba(180, 180, 30, 0.8)");
+
+    //
+    // filtering methods //////////////////
+    // $("#list")
+    //     .find("li")
+    //     .filter(":even")
+    //     .css("background-color", "rgba(180, 180, 30, 0.8)");
+    // $("#list")
+    //     .children("li")
+    //     .filter(":even")
+    //     .css("background-color", "rgba(180, 180, 30, 0.8)");
+    // $("li")
+    //     .filter(function(index) {
+    //         return index % 3 === 2;
+    //     })
+    //     .css("background-color", "rgba(180, 180, 30, 0.8)");
+
+    // $("li")
+    //     .first()
+    //     .css("background-color", "rgba(180, 180, 30, 0.8)");
+    // $("li")
+    //     .last()
+    //     .css("background-color", "rgba(180, 180, 30, 0.8)");
+    // $("li")
+    //     .eq(0)
+    //     .css("background-color", "rgba(180, 180, 30, 0.8)");
+    // $("li")
+    //     .not(":last")
+    //     .css("background-color", "rgba(180, 180, 30, 0.8)");
+    // $("li")
+    //     .not(function(index) {
+    //         return index % 3 === 1;
+    //     })
+    //     .css("background-color", "rgba(180, 180, 30, 0.8)");
+    $("li")
+        .not("#list ul li")
         .css("background-color", "rgba(180, 180, 30, 0.8)");
 });
