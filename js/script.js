dc6d9b3ca5138708730a3dbfd17c6090003a42e7
Marc Turner <marc.t1967@gmail.com>
2019-11-21 21:01:46 -0500

    using css selectors

--- js/script.js
+++ js/script.js
@@ -53,8 +53,23 @@ $(function() {
     //         );
     //     });
     // });
+    // $(".lightbox")
+    //     .delay(500)
+    //     .fadeIn(1000);
 
-    $(".lightbox")
-        .delay(500)
-        .fadeIn(1000);
+    // $("p").css("background-color", "rgba(180, 180, 30, 0.8)");
+    // $(".red-box").css("background-color", "rgba(180, 180, 30, 0.8)");
+    // $("#list").css("background-color", "rgba(180, 180, 30, 0.8)");
+    // $("input[type = 'text']").css(
+    //     "background-color",
+    //     "rgba(180, 180, 30, 0.8)"
+    //);
+    // $("h2, p, input").css("background-color", "rgba(180, 180, 30, 0.8)");
+    // $("p:first").css("background-color", "rgba(180, 180, 30, 0.8)");
+    // $("li:first").css("background-color", "rgba(180, 180, 30, 0.8)");
+    // $("li:last").css("background-color", "rgba(180, 180, 30, 0.8)");
+    // $("li:even").css("background-color", "rgba(180, 180, 30, 0.8)");
+    // $("li:odd").css("background-color", "rgba(180, 180, 30, 0.8)");
+
+    $("input:text").css("background-color", "rgba(180, 180, 30, 0.8)");
 });
