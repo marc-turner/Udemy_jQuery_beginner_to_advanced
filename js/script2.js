a7679fd91b3efd65b6e8f1e3b48639bf3ec590b4
Marc Turner <marc.t1967@gmail.com>
2019-12-02 22:06:52 -0500

    adding elements to the DOM

--- js/script2.js
+++ js/script2.js
@@ -1,3 +1,27 @@
-$(function() {
-    $("ul ul:first").append("<li> new item</li>");
+$(function () {
+    // $("ul ul:first").append("<li>append new item</li>");
+
+    // $("<li>appendTo new item</li>").appendTo($("ul ul:first"));
+
+    // $("ul ul:first").prepend("<li>prepend new item</li>");
+
+    // $("<h4> Marc Turner </h4>").prependTo("#content");
+
+    // $(".red-box").after("<div class='red-box'>Another box</div>");
+    // $(".blue-box").before("<div class='blue-box'>Another box</div>");
+
+    // $(".blue-box").before(function () {
+    //     return "<div class='blue-box'>Blue Box 2</div>";
+    // });
+
+    // $(".blue-box").before($(".red-box"));
+    // $(".blue-box").after($(".green-box"));
+
+    // $("p").after($("#list"));
+
+    // $(".red-box").after("<div class='green-box'>Green Box 2</div>");
+    // $(".green-box").last().after("<div class='blue-box'>Blue Box 2</div>");
+    // $(".blue-box").last().after("<div class='red-box'>Red Box 2</div");
+
+    $("li").replaceWith("<li>replaced</li>");
 });
